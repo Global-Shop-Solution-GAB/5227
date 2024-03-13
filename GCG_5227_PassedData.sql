e245635d5b05ee212d24898ec95dafbaa85b0753
Service Web <sw@gssmail.com>
2024-03-13 13:47:08 -0500

    Commit changes in C:\Websites\Git\emarlow\5227\GCG_5227_PassedData.sql

---
+++ GCG_5227_PassedData.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_5227_PassedData
+ * ========================================== */
+CREATE TABLE "GCG_5227_PassedData"(
+ "ID" IDENTITY DEFAULT '0',
+ "TERMINAL" VARCHAR(3),
+ "DATA" LONGVARCHAR,
+ "ORDER_NO" CHAR(7),
+ UNIQUE ("ID"));
+
+
