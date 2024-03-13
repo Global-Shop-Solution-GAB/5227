9b5bdb8fe99d9a19030438d47e1654e45b36f1dc
Service Web <sw@gssmail.com>
2024-03-13 13:47:08 -0500

    Commit changes in C:\Websites\Git\emarlow\5227\GCG_5227_Templates.sql

---
+++ GCG_5227_Templates.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5227_Templates
+ * ========================================== */
+CREATE TABLE "GCG_5227_Templates"(
+ "ID" IDENTITY DEFAULT '0',
+ "SUBJECT" CHAR(200),
+ "BODY" CHAR(1000),
+ UNIQUE ("ID"));
+
+
