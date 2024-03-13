6e1d2c036d7277ad6259345966ba10f8dd6d40d4
Service Web <sw@gssmail.com>
2024-03-13 13:47:08 -0500

    Commit changes in C:\Websites\Git\emarlow\5227\GCG_5227_Emails.sql

---
+++ GCG_5227_Emails.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5227_Emails
+ * ========================================== */
+CREATE TABLE "GCG_5227_Emails"(
+ "ID" IDENTITY DEFAULT '0',
+ "Email" CHAR(50),
+ UNIQUE ("ID"));
+
+
